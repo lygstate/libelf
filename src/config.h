19cd0b5d537bb9bdb7b165e114bc2ce272d94a7d
Yonggang Luo <luoyonggang@gmail.com>
2022-09-18 22:39:38 +0800

    win32 don't have MEMRCHR

--- src/config.h
+++ src/config.h
@@ -1,8 +1,11 @@
+#ifndef _CONFIG_H_
 #define _GNU_SOURCE
 #include "eu-config.h"
 
 #define HAVE_DECL_MEMPCPY 1
+#ifndef _WIN32
 #define HAVE_DECL_MEMRCHR 1
+#endif
 #define HAVE_DECL_POWEROF2 1
 #define HAVE_DECL_RAWMEMCHR 0
 #define HAVE_DECL_REALLOCARRAY 1
@@ -10,3 +13,4 @@
 
 #undef HAVE_GCC_STRUCT
 #undef USE_LOCKS
+#endif
